e4de51e8849bb4f4472200161bbbcf6e0a6fc704
TOR968 <tor968@yahoo.com>
2020-12-21 21:38:11 +0200

    Update App.js

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,62 +1,37 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section/Section';
 import ContactForm from './ContactForm/ContactForm ';
 import ContactList from './ContactList/ContactList ';
 import Filter from './Filter/Filter';
 
+import useLocalStorage from '../components/hooks/useLocalStorage';
+
 import { toast, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+function App() {
+  const [contacts, setContacts] = useLocalStorage('userContacts', [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  const changeHandler = e => {
+    const { value } = e.target;
+
+    setFilter(value);
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevState) {
-    const nextContacts = this.state.contacts;
-    const prevContacts = prevState.contacts;
-
-    if (nextContacts !== prevContacts) {
-      localStorage.setItem('contacts', JSON.stringify(nextContacts));
-    }
-  }
-
-  changeHandler = e => {
-    const { name, value } = e.target;
-
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  deleteContactbyId = id => {
-    const { contacts } = this.state;
+  const deleteContactbyId = id => {
     const updatedContacts = contacts.filter(contact => contact.id !== id);
-    this.setState({
-      contacts: [...updatedContacts],
-    });
+    setContacts([...updatedContacts]);
   };
 
-  addContact = contact => {
+  const addContact = contact => {
     const newName = contact.name;
-    const names = this.state.contacts.map(contact =>
-      contact.name.toLowerCase(),
-    );
+    const names = contacts.map(contact => contact.name.toLowerCase());
     if (names.includes(newName.toLowerCase().trim())) {
       toast.configure();
       toast.error(`${newName} is already in contact list`, {
@@ -66,14 +41,11 @@ class App extends Component {
         transition: Zoom,
       });
     } else {
-      this.setState(state => ({
-        contacts: [...state.contacts, contact],
-      }));
+      setContacts([...contacts, contact]);
     }
   };
 
-  filterContactsByName = () => {
-    const { contacts, filter } = this.state;
+  const filterContactsByName = () => {
     if (contacts.length) {
       return contacts.filter(contact =>
         contact.name.toLowerCase().includes(filter.toLowerCase()),
@@ -81,23 +53,20 @@ class App extends Component {
     }
   };
 
-  render() {
-    const { contacts } = this.state;
-    return (
-      <>
-        <Section title="Phonebook">
-          <ContactForm addContact={this.addContact} />
-        </Section>
-        <Section title="Contacts">
-          {contacts.length > 1 && <Filter onChange={this.changeHandler} />}
-          <ContactList
-            contacts={this.filterContactsByName()}
-            onDelete={this.deleteContactbyId}
-          />
-        </Section>
-      </>
-    );
-  }
+  return (
+    <>
+      <Section title="Phonebook">
+        <ContactForm addContact={addContact} />
+      </Section>
+      <Section title="Contacts">
+        {contacts.length > 1 && <Filter onChange={changeHandler} />}
+        <ContactList
+          contacts={filterContactsByName()}
+          onDelete={deleteContactbyId}
+        />
+      </Section>
+    </>
+  );
 }
 
 export default App;
